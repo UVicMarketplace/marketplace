b8cdece7947e38c7df00057fd236b0406247d108
Anika Bookout <anbookout@gmail.com>
2024-06-18 16:43:35 -0700

    Message page basic frontend functionality done

--- apps/frontend/src/styles/pageStyles.tsx
+++ apps/frontend/src/styles/pageStyles.tsx
@@ -200,11 +200,14 @@ export const useStyles = () => ({
   messagesMessagesBox: {
     height: "100%",
     overflowY: "scroll",
-    "& .infinite-scroll-component": {
-      display: "flex",
-      flexDirection: "column-reverse" as "column-reverse",
-      alignItems: "center",
-      gap: "1rem",
+
+    display: "flex",
+    flexDirection: "column-reverse" as "column-reverse",
+    alignItems: "center",
+    gap: "1rem",
+
+    "& :first-child": {
+      marginBottom: "0.25rem",
     },
   },
   messagesSendBox: {
@@ -220,8 +223,8 @@ export const useStyles = () => ({
     color: colors.martletplaceWhite,
     padding: "0.5rem",
     maxWidth: "80vw",
-    borderRadius: "8px",
-    borderBottomRightRadius: "0px",
+    borderRadius: "10px",
+    borderBottomRightRadius: "2px",
     marginInline: "1rem",
   },
   messageFromOther: {
@@ -230,8 +233,8 @@ export const useStyles = () => ({
     color: colors.martletplaceWhite,
     padding: "0.5rem",
     maxWidth: "80vw",
-    borderRadius: "8px",
-    borderBottomLeftRadius: "0px",
+    borderRadius: "10px",
+    borderBottomLeftRadius: "2px",
     marginInline: "1rem",
   },
 });
