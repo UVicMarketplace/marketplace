62039fbb3e9c2b2df9dc8e5e66f4e80d40e6e0bf
Anika Bookout <anbookout@gmail.com>
2024-06-26 14:03:41 -0700

    working on making linter happy

--- apps/frontend/src/styles/pageStyles.tsx
+++ apps/frontend/src/styles/pageStyles.tsx
@@ -198,7 +198,7 @@ export const useStyles = () => ({
     overflowY: "scroll",
 
     display: "flex",
-    flexDirection: "column-reverse" as "column-reverse",
+    flexDirection: "column-reverse" as const,
     alignItems: "center",
     gap: "1rem",
 
@@ -237,7 +237,7 @@ export const useStyles = () => ({
   },
   messagesConvBox: {
     display: "flex",
-    flexDirection: "row" as "row",
+    flexDirection: "row" as const,
     alignItems: "center",
     justifyContent: "space-between",
     gap: "1rem",
